4842c7f4ead43948997f1fd7e4b7292c0e3c40a3
danny24785 <dannyhalbesma@gmail.com>
2016-04-29 15:40:15 +0200

    new files added

---
+++ js/script5.js
@@ -0,0 +1,9 @@
+// Script for #5
+
+$(window).on('load', function(){
+	alert('the window has loaded');
+});
+
+$(document).ready(function(){
+	alert('The DOM is ready');
+});
---
+++ js/script6.js
@@ -0,0 +1,4 @@
+// Script for #6
+
+var count = $('#a_list').find('*').length;
+alert(count);
