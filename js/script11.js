1a5d8a7df166681ea9259997b27cc587cebbe76c
danny24785 <dannyhalbesma@gmail.com>
2016-04-30 22:42:54 +0200

    added #11

---
+++ js/script11.js
@@ -0,0 +1,9 @@
+// Script for #11
+
+/******** script according to Quentin */
+$('p').click(function(){
+	$(this).text('that tickles!');
+});
+/********/
+
+/******** same script rewritten according to Peter Kassenaar ********/
