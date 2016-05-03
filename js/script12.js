b058b1e9add5aea3ddd9eb468476c4a57e27580c
danny24785 <dannyhalbesma@gmail.com>
2016-05-03 22:25:36 +0200

    added files for #12

--- js/script12.js
+++ js/script12.js
@@ -1,7 +1,7 @@
 // Script for #12
 
 /******** script according to Quentin */
-$('').
+$("p:contains('J')").css('background-color', 'yellow');
 /********/
 
 /******** same script rewritten according to Peter Kassenaar ********/
