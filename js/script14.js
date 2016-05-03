3320e39a727ab260a12e4df7f9612f60cdd00a7c
danny24785 <dannyhalbesma@gmail.com>
2016-05-03 22:56:56 +0200

    added files

--- js/script14.js
+++ js/script14.js
@@ -1,7 +1,15 @@
 // Script for #12
 
 /******** script according to Quentin */
+// single click
+$('#image_toggler').click(function(){
+	$('#car').toggle();
+});
 
+// double click
+$('#image_toggler_dbl').dblclick(function(){
+	$('#car').toggle();
+});
 /********/
 
 /******** same script rewritten according to Peter Kassenaar ********/
